99b3d678a6a1e922f7816481f2d0d1d312a89b8c
Gaurav Kapoor <gaurav.kapoor2@mail.dcu.ie>
2017-02-26 13:46:46 +0000

    Slider added

--- src/ImageViewer.java
+++ src/ImageViewer.java
@@ -3,19 +3,30 @@ import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.JSlider;
 
 /** This class is an entry point of the simple image viewer.
  */
-public class ImageViewer extends javax.swing.JFrame {
+public class ImageViewer extends JFrame {
     // Variables declaration
-    private javax.swing.JMenuItem clearImageArea;
-    private javax.swing.JMenuItem openImageExplorer;
-    private javax.swing.JMenu imageMenu;
-    private javax.swing.JMenuBar imageMenuBar;
-    private javax.swing.JScrollPane imageScroller;
+    private JMenuItem clearImageArea;
+    private JMenuItem openImageExplorer;
+    private JMenu imageMenu;
+    private JMenuBar imageMenuBar;
+    private JScrollPane imageScroller;
+    private JSlider imageSlider;
     //create a label
     JLabel jlab = new JLabel();
+    // Slider variables
+    static int SLIDE_MIN = 0;
+    static int SLIDE_MAX = 30;
+    static int SLIDE_INIT = 0;
     // End of variables declaration
     /**
      * ImageViewer constructor which initialize the image frame with components
@@ -29,11 +40,12 @@ public class ImageViewer extends javax.swing.JFrame {
      */
     @SuppressWarnings("unchecked")
     private void initComponents() {
-        imageScroller = new javax.swing.JScrollPane();
-        imageMenuBar = new javax.swing.JMenuBar();
-        imageMenu = new javax.swing.JMenu();
-        openImageExplorer = new javax.swing.JMenuItem();
-        clearImageArea = new javax.swing.JMenuItem();
+        imageScroller = new JScrollPane();
+        imageMenuBar = new JMenuBar();
+        imageMenu = new JMenu();
+        openImageExplorer = new JMenuItem();
+        clearImageArea = new JMenuItem();
+      
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         imageMenu.setText("File");
         openImageExplorer.setText("Open");
@@ -42,8 +54,9 @@ public class ImageViewer extends javax.swing.JFrame {
                 openImageExplorerActionPerformed(evt);
             }
         });
+        //Image slider.
+        imageSlider = new JSlider(JSlider.HORIZONTAL, SLIDE_MIN, SLIDE_MAX, SLIDE_INIT);
         imageMenu.add(openImageExplorer);
-
         clearImageArea.setText("Clear");
         clearImageArea.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -56,15 +69,27 @@ public class ImageViewer extends javax.swing.JFrame {
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(imageScroller, javax.swing.GroupLayout.DEFAULT_SIZE, 1200, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(imageScroller, javax.swing.GroupLayout.DEFAULT_SIZE, 700, Short.MAX_VALUE)
-        );
 
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addContainerGap(25, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(imageScroller, javax.swing.GroupLayout.DEFAULT_SIZE, 1200, Short.MAX_VALUE)
+                        )
+                    .addComponent(imageSlider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    )
+            );
+            layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addContainerGap(25, Short.MAX_VALUE)
+                    .addComponent(imageScroller, javax.swing.GroupLayout.DEFAULT_SIZE, 700, Short.MAX_VALUE)
+                    .addGap(143, 143, 143))
+                .addGroup(layout.createSequentialGroup()
+                    .addComponent(imageSlider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    )
+            );
         pack();
     }
 
@@ -75,6 +100,7 @@ public class ImageViewer extends javax.swing.JFrame {
         if(chooser.showOpenDialog(imageMenu) == JFileChooser.APPROVE_OPTION){
             //get selected image files
             File[] fileSelections = chooser.getSelectedFiles();
+            SLIDE_MAX = fileSelections.length;
             for(File f : fileSelections) {
             	System.out.println(f.getName());
             }
@@ -90,6 +116,7 @@ public class ImageViewer extends javax.swing.JFrame {
             jlab.setHorizontalAlignment(JLabel.CENTER);
             //add jLabel to scroll pane
             imageScroller.getViewport().add(jlab);
+            //imageScroller.getViewport().add(imageSlider);
         }
     }
 
